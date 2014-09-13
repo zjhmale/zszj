b119a5ad685e637d8373488ca50c3c830667f2fa
zjhmale <zjhmale@gmail.com>
2014-09-13 17:26:53 +0800

    init project add some dependencies

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject zszj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [korma "0.3.2"]
+                 [mysql/mysql-connector-java "5.1.6"]
+                 [ring-server "0.3.1"]
+                 [compojure "1.1.8"]
+                 [selmer "0.7.1"]
+                 [environ "0.5.0"]
+                 [enlive "1.1.5"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler zszj.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
