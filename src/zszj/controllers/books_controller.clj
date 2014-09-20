433c2082c6876729c5b2a8211c7d638316196032
zjhmale <zjhmale@gmail.com>
2014-09-20 15:12:50 +0800

    format template files and not finish with books

--- src/zszj/controllers/books_controller.clj
+++ src/zszj/controllers/books_controller.clj
@@ -4,12 +4,19 @@
             [zszj.db.core :as db]
             [zszj.views.helper :as helper]))
 
+(def ^:dynamic *PER-PAGE* 20)
+
+(def PER-PAGE 20)
+
 (defn index
   [& params]
   ;;(println params)
   (let [book_type (-> (nth params 0) :book_type)]
     (println book_type)
-    "cleantha"))
+    (layout/render "books/index.html"
+                   (common/common-manipulate
+                    (merge {}
+                           (common/paginator 20 PER-PAGE 1 (str "/books"))) "wssx"))))
 
 (defn search
   [& ajaxargs]
