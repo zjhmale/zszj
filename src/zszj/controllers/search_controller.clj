75865fbf0aace79aa91356f04aebc56fee12fa9d
zjhmale <zjhmale@gmail.com>
2015-07-10 12:29:36 +0800

    finish highlight search text in search result list

--- src/zszj/controllers/search_controller.clj
+++ src/zszj/controllers/search_controller.clj
@@ -3,16 +3,33 @@
             [zszj.controllers.common :as common]
             [zszj.db.full-texts :as fulltexts]))
 
+(defn highlight-search-text
+  [full_texts search_str]
+  (map (fn [fulltext]
+         (let [offset 60
+               parts (clojure.string/split (:full_text fulltext) (re-pattern search_str))
+               pretext-raw (nth parts 0)
+               followtext-raw (nth parts 1)
+               preindex (- (count pretext-raw) offset)
+               pretext (if (> preindex 0)
+                         (str "... " (subs pretext-raw preindex))
+                         pretext-raw)
+               followtext (if (< offset (count followtext-raw))
+                            (str (subs followtext-raw 0 offset) " ...")
+                            followtext-raw)]
+           (-> fulltext
+               (assoc :pretext pretext)
+               (assoc :followtext followtext)))) full_texts))
+
 (defn index
   [request]
   (let [search_str (get-in request [:params :search :search_str])
         search_cat (get-in request [:params :search :search_cat])
-        full_texts (fulltexts/search-by-cat search_str search_cat)]
+        full_texts (highlight-search-text
+                     (fulltexts/search-by-cat search_str search_cat)
+                     search_str)]
     (layout/render "search/index.html"
                    (common/common-manipulate
                      {:cats       (fulltexts/get-cats)
                       :full_texts full_texts
-                      :search_str search_str
-                      :pretext    "pretext"
-                      :highlight  "高亮"
-                      :followtext "followtext"} ""))))
+                      :search_str search_str} ""))))
--- test/zszj/test/db.clj
+++ test/zszj/test/db.clj
@@ -1,6 +1,7 @@
 (ns zszj.test.db
   (:require [clojure.test :refer :all]
-            [zszj.db.full-texts :as fulltexts]))
+            [zszj.db.full-texts :as fulltexts]
+            [zszj.controllers.search-controller :as searchcontroller]))
 
 (deftest test-get-cats
   (testing "get all categories from full_texts table"
@@ -9,3 +10,9 @@
 (deftest test-search-by-cat
   (testing "search from full_texts table by search_str and search_cat"
     (prn (fulltexts/search-by-cat "2015" "招标代理机构"))))
+
+(deftest test-highlight-search-text
+  (testing "hight the search text in full text"
+    (let [search_str "工程造价"
+          fulltexts [{:full_text "我委组织召开贯彻新版施工合同（示范文本）工作座谈会 \n 近日，我委与市工商行政管理局联合印发了&ldquo;转发关于做好《建设工程施工合同（示范文本）》（GF-2013-0201）的通知&rdquo;（舟建委[2013]151号），自今年8月1日起，我市将全面贯彻执行2013版施工合同（示范文本）。为进一步掌握2013版施工合同（示范文本）精神，我委于29日组织召开了贯彻2013版施工合同（示范文本）工作座谈会。委政策法规处，建筑业管理处，市造价站、市招标办负责人，各县（区）建设主管部门、金塘与六横管委会分管领导与相关科室负责人及全市工程造价咨询、招标代理机构（含分支机构）企业负责人参加了座谈会。\n\n 市住建委党委委员、市城管执法局胡云龙副局长在会上从施工合同（示范文本）修改的背景、修改的原则和修改的特点三方面强调充分认识2013版施工合同（示范文本）的作用和意义，并就如何贯彻实施2013版施工合同（示范文本）提出三点意见：一是要加强2013版施工合同（示范文本）宣贯工作；二是要加强施工合同履约管理，三是要市场各方主体自觉实施2013版施工合同（示范文本）。\n\n\n\n"}]]
+      (prn (searchcontroller/highlight-search-text fulltexts search_str)))))
