04d09812df4495e4872b1bc4fcc24974bb10b0f3
zjhmale <zjhmale@gmail.com>
2015-07-10 09:58:52 +0800

    实现高亮搜索的结构还要截取带有搜索结果的一部分正文需要自己实现 之前rails的项目中直接用的现成的view helper函数

--- src/zszj/controllers/search_controller.clj
+++ src/zszj/controllers/search_controller.clj
@@ -1,10 +1,21 @@
 (ns zszj.controllers.search-controller
-  (:require [zszj.views.layout :as layout]))
+  (:require [zszj.views.layout :as layout]
+            [zszj.controllers.common :as common]
+            [zszj.db.full-texts :as fulltexts]))
 
 (defn index
   []
-  (layout/render "search/index.html"))
+  (layout/render "search/index.html"
+                 (common/common-manipulate
+                   {:type       "search"
+                    :cats       (fulltexts/get-cats)
+                    :pretext    "pretext"
+                    :highlight  "高亮"
+                    :followtext "followtext"} "")))
 
 (defn search
   []
-  (layout/render "search/index.html"))
+  (layout/render "search/index.html"
+                 (common/common-manipulate
+                   {:type "search"
+                    :cats (fulltexts/get-cats)} "")))
